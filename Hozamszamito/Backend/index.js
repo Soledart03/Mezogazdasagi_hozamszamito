c362ac5bb47f01f5b099151877d63dd280f1091c
Koszta Máté <116151165+Soledart03@users.noreply.github.com>
2025-11-19 19:49:42 +0100

    v2

--- Hozamszamito/Backend/index.js
+++ Hozamszamito/Backend/index.js
@@ -21,6 +21,7 @@ app.get('/api/gazda', (req, res) => {
  res.json(results);
  });
 });
+//szi apu
 app.get('/api/gazdkiad', (req, res) => {
  db.query('SELECT g.nev,f.muvelesi_ag,k.tipus FROM gazda_fiok AS g JOIN fold AS f ON g.id = f.gazda_id JOIN kiadas AS k ON f.id = k.fold_id ', (err, results) => {
  if (err) throw err;
