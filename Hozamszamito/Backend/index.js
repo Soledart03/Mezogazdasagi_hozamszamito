fe78193d557ec6ea06ee11aa4e570a2ebe9bc83b
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2025-11-19 20:24:05 +0100

    vn

--- Hozamszamito/Backend/index.js
+++ Hozamszamito/Backend/index.js
@@ -15,13 +15,14 @@ db.connect(err => {
  if (err) throw err;
  console.log('MySQL kapcsolódva.');
 });
+//innen kezd
 app.get('/api/gazda', (req, res) => {
  db.query('SELECT * FROM gazda_fiok', (err, results) => {
  if (err) throw err;
  res.json(results);
  });
 });
-//szevasz gecim
+//idáig
 app.get('/api/gazdkiad', (req, res) => {
  db.query('SELECT g.nev,f.muvelesi_ag,k.tipus FROM gazda_fiok AS g JOIN fold AS f ON g.id = f.gazda_id JOIN kiadas AS k ON f.id = k.fold_id ', (err, results) => {
  if (err) throw err;
