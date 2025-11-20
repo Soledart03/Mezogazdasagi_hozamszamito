06af7355e0f36d9c9cd7b04d5091de8e5379028c
Koszta Máté <116151165+Soledart03@users.noreply.github.com>
2025-11-20 19:03:18 +0100

    v4

--- Hozamszamito/Backend/index.js
+++ Hozamszamito/Backend/index.js
@@ -35,6 +35,49 @@ app.get('/api/novenyadat', (req, res) => {
  res.json(results);
  });
 });
+
+//Koszta1
+app.get('/api/elozoev', (req, res) => {
+ db.query('SELECT f.elozo_evi_hasznositas FROM fold AS f JOIN gazda_fiok AS g ON f.gazda_id = g.id WHERE g.nev = `Kovács Péter` ;', (err, results) => {
+ if (err) throw err;
+ res.json(results);
+ });
+});
+
+//Koszta2
+app.get('/api/gazdanev', (req, res) => {
+ db.query('SELECT g.nev FROM gazda_fiok AS g JOIN fold AS f ON f.gazda_id = g.id WHERE f.helyrajzi_szam = 9101/5 ;', (err, results) => {
+ if (err) throw err;
+ res.json(results);
+ });
+});
+
+//Koszta3
+app.get('/api/veti', (req, res) => {
+ db.query('SELECT t.vetes_idopont FROM terv AS t WHERE t.fold_id = 1 ;', (err, results) => {
+ if (err) throw err;
+ res.json(results);
+ });
+});
+
+//Koszta4
+app.get('/api/datumbuza', (req, res) => {
+ db.query('SELECT k.datum FROM kiadas AS k JOIN noveny AS n ON k.noveny_id = n.id WHERE n.nnev like `Búza`', (err, results) => {
+ if (err) throw err;
+ res.json(results);
+ });
+});
+
+//Koszta5
+app.get('/api/termikili', (req, res) => {
+ db.query('SELECT ni.termes_per_kilo FROM noveny AS n JOIN noveny_input AS ni ON ni.noveny_id = n.id WHERE n.nnev = `Búza`', (err, results) => {
+ if (err) throw err;
+ res.json(results);
+ });
+});
+
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
  console.log(`Server running on http://localhost:${PORT}`);
