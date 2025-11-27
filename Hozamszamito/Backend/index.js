4faa5a5df05ec380fa0b1aa4c5192576ba165726
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2025-11-27 11:00:25 +0100

    v_1

--- Hozamszamito/Backend/index.js
+++ Hozamszamito/Backend/index.js
@@ -15,97 +15,19 @@ db.connect(err => {
  if (err) throw err;
  console.log('MySQL kapcsolódva.');
 });
-//innen kezd
-app.get('/api/gazda', (req, res) => {
+const apiurl = '/api/gazda';
+
+app.get(apiurl, (req, res) => {
  db.query('SELECT * FROM gazda_fiok', (err, results) => {
  if (err) throw err;
  res.json(results);
  });
 });
-//idáig
-//1
-app.get('/api/gazdkiad', (req, res) => {
- db.query('SELECT g.nev,f.muvelesi_ag,k.tipus FROM gazda_fiok AS g JOIN fold AS f ON g.id = f.gazda_id JOIN kiadas AS k ON f.id = k.fold_id', (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-//2
-app.get('/api/novenyadat', (req, res) => {
- db.query('SELECT n.nnev, i.inev, ni.termes_per_kilo FROM noveny AS n JOIN noveny_input AS ni ON n.id = ni.noveny_id JOIN input_anyag AS i ON i.id = ni.inputanyag_id', (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-//3(new)
-app.get('/api/vetesiidopont', (req, res) => {
- db.query(`SELECT gf.email, f.helyrajzi_szam, t.vetes_idopont FROM gazda_fiok AS gf JOIN fold AS f ON f.gazda_id = gf.id JOIN terv AS t ON t.fold_id = f.id;`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
 
-//4(new)
-app.get('/api/tipi', (req, res) => {
- db.query(`SELECT n.nnev AS noveny_nev, t.tomeg, k.tipus FROM noveny AS n LEFT JOIN terv AS t ON t.noveny_id = n.id LEFT JOIN kiadas AS k ON k.noveny_id = n.id;`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-
-//5(nincs kész és nem is lesz mostanában :))
-app.get('/api/vetesiidopont', (req, res) => {
- db.query(`SELECT gf.email, f.helyrajzi_szam, t.vetes_idopont FROM gazda_fiok AS gf JOIN fold AS f ON f.gazda_id = gf.id JOIN terv AS t ON t.fold_id = f.id;`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-
-
-//Koszta1
-app.get('/api/elozoev', (req, res) => {
- db.query(`SELECT f.elozo_evi_hasznositas FROM fold AS f JOIN gazda_fiok AS g ON f.gazda_id = g.id WHERE g.nev = 'Kovács Péter'`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-
-//Koszta2
-app.get('/api/gazdanev', (req, res) => {
- db.query(`SELECT g.nev FROM gazda_fiok AS g JOIN fold AS f ON f.gazda_id = g.id WHERE f.helyrajzi_szam = '9101/5'`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-
-//Koszta3
-app.get('/api/veti', (req, res) => {
- db.query('SELECT t.vetes_idopont FROM terv AS t JOIN fold AS f ON t.fold_id = f.id WHERE t.fold_id = 1', (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-
-//Koszta4
-app.get('/api/datumbuza', (req, res) => {
- db.query(`SELECT k.datum FROM kiadas AS k JOIN noveny AS n ON k.noveny_id = n.id WHERE n.nnev LIKE 'Búza'`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-
-//Koszta5
-app.get('/api/termikili', (req, res) => {
- db.query(`SELECT n.nnev, ni.termes_per_kilo FROM noveny_input AS ni JOIN noveny AS n ON ni.noveny_id = n.id WHERE n.nnev LIKE 'Búza'`, (err, results) => {
- if (err) throw err;
- res.json(results);
- });
-});
-// ------------------ POST New Game ------------------
-app.post('/api/gazdafel', (req, res) => {
+app.post(apiurl, (req, res) => {
     const { nev, email, jelszo} = req.body;
     db.query(
-    'INSERT INTO games (nev, email, jelszo) VALUES (?, ?, ?)',
+    'INSERT INTO gazda_fiok (nev, email, jelszo) VALUES (?, ?, ?)',
     [nev, email, jelszo],
     (err, results) => {
     if (err) throw err;
@@ -113,30 +35,7 @@ app.post('/api/gazdafel', (req, res) => {
     }
     );
    });
-// ------------------ PUT Update Game ------------------
-app.put('/api/games/:id', (req, res) => {
-    const id = req.params.id;
-    const { title, genre, publisher, release_year, rating } = req.body;
-    db.query(
-    'UPDATE games SET title=?, genre=?, publisher=?, release_year=?, rating=? WHERE id=?',
-    [title, genre, publisher, release_year, rating, id],
-    (err) => {
-    if (err) throw err;
-    res.json({ message: 'Game updated' });
-    }
-    );
-   });
-   // ------------------ DELETE Game ------------------
-   app.delete('/api/games/:id', (req, res) => {
-    const id = req.params.id;
-    db.query('DELETE FROM games WHERE id=?', [id], (err) => {
-    if (err) throw err;
-    res.json({ message: 'Game deleted' });
-    });
-   });
    
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
  console.log(`Server running on http://localhost:${PORT}`);
--- Hozamszamito/Frontend/src/app/gazdaservice.ts
+++ Hozamszamito/Frontend/src/app/gazdaservice.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class Gazdaservice {
-  
+  private api = 'http://localhost:3000/api/gazda';
 }
