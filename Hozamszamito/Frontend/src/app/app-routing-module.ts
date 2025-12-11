de71ec8e6a2b7d1fb26b2472aea37f04222f8933
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2025-12-11 10:51:44 +0100

    v_homepage_st

--- Hozamszamito/Frontend/src/app/app-routing-module.ts
+++ Hozamszamito/Frontend/src/app/app-routing-module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Regcomp } from './regcomp/regcomp';
 import { Fooldal } from './fooldal/fooldal';
+import { Loginform } from './loginform/loginform';
 
 const routes: Routes = [
   {path:'register', component:Regcomp },
+  {path:'login',component:Loginform},
   {path:'',component:Fooldal}
 ];
 
