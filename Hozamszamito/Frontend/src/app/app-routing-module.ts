f848b46e060966e23290936ffd2b16e0b2a04777
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2025-12-27 14:48:20 +0100

    v_fold_2

--- Hozamszamito/Frontend/src/app/app-routing-module.ts
+++ Hozamszamito/Frontend/src/app/app-routing-module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { Regcomp } from './regcomp/regcomp';
 import { Fooldal } from './fooldal/fooldal';
 import { Loginform } from './loginform/loginform';
-
+import { Foldek } from './foldek/foldek';
 const routes: Routes = [
   {path:'register', component:Regcomp },
   {path:'login',component:Loginform},
-  {path:'',component:Fooldal}
+  {path:'',component:Fooldal},
+  {path:'fold',component:Foldek}
 ];
 
 @NgModule({
