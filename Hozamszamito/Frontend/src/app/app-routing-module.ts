798f40ee0dfb5f0e10f315d485491db0f9393fdc
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2025-12-27 15:20:57 +0100

    v_foldfel_fix2

--- Hozamszamito/Frontend/src/app/app-routing-module.ts
+++ Hozamszamito/Frontend/src/app/app-routing-module.ts
@@ -4,10 +4,12 @@ import { Regcomp } from './regcomp/regcomp';
 import { Fooldal } from './fooldal/fooldal';
 import { Loginform } from './loginform/loginform';
 import { Foldek } from './foldek/foldek';
+import { Foldfelform } from './foldfelform/foldfelform';
 const routes: Routes = [
   {path:'register', component:Regcomp },
   {path:'login',component:Loginform},
   {path:'',component:Fooldal},
+  {path:'foldfelvisz',component:Foldfelform},
   {path:'fold',component:Foldek}
 ];
 
