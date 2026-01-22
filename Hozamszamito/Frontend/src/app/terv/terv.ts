e016e8dfa4f28e3ab571fc654167d4dc927a3f4c
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2026-01-22 11:37:46 +0100

    v_nemmegy_1

--- Hozamszamito/Frontend/src/app/terv/terv.ts
+++ Hozamszamito/Frontend/src/app/terv/terv.ts
@@ -3,6 +3,8 @@ import { App } from '../app';
 import { Gazdaservice } from '../gazdaservice';
 import { Tervservice } from '../tervservice';
 import { Observable } from 'rxjs';
+import { Foldservice } from '../foldservice';
+
 @Component({
   selector: 'app-terv',
   standalone: false,
@@ -10,6 +12,57 @@ import { Observable } from 'rxjs';
   styleUrl: './terv.css',
 })
 export class Terv {
-constructor(private app:App,private gazdaser:Gazdaservice,private tervser:Tervservice){}
+constructor(private app:App,private gazdaser:Gazdaservice,private tervser:Tervservice,private foldser:Foldservice){}
+terv$!: Observable<any[]>;
+folds$!: Observable<any[]>;
+foldId: number = 0;
 gazdaId: number = 0;
+terv:any = {id:0,fold_id:0,noveny_id:0,vetes_idopont:'',tomeg:0,osszeg:0}
+fold:any = {id:0,terulet:'',muvelesi_ag:'',helyrajzi_szam:'',elozo_evi_hasznositas:'',g_id:0};
+editing:boolean=false;
+ngOnInit() {
+  this.folds$ = this.foldser.fold$;
+
+  this.gazdaser.gazda$.subscribe(gazda => {
+    this.gazdaId = gazda?.id ?? 0;
+
+    if (this.gazdaId !== 0) {
+      this.foldser.loadFoldsByGazdaId(this.gazdaId);
+    }
+
+    console.log('gazdaId:', this.gazdaId);
+  });
+
+  this.foldser.fold$.subscribe(folds => {
+    this.foldId = folds?.[0]?.id ?? 0;
+    console.log('foldId:', this.foldId);
+  });
+}
+selectedTerv: any = null;
+
+openEdit(terv: any) {
+  
+  this.selectedTerv = { ...terv };
+  this.editing = true;
+}
+/*
+delTerv(terv: any) {
+  this.tervser.deleteTerv(terv.id);
+  this.tervser.loadTervByFold(this.gazdaId);
+  window.alert("Tervezet törölve!")
+}
+
+  save() {
+  this.tervser.updateTerv(this.selectedTerv);
+  this.editing = false;
+}
+cancel() {
+  this.editing = false;
+  this.tervser.loadTervByFold(this.gazdaId);
+}
+  */
+openMenu(type: any){
+    this.app.openMenu(type);
+  }
+
 }
--- Hozamszamito/Frontend/src/app/tervservice.ts
+++ Hozamszamito/Frontend/src/app/tervservice.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+const terv = 'foldlista';
 @Injectable({
   providedIn: 'root',
 })
 export class Tervservice {
-  
+  constructor(private http:HttpClient){}
+  private tervSubject = new BehaviorSubject<any[]>(this.loadFromStorage());
+  terv$ = this.tervSubject.asObservable();
+  loadTervByFold(foldID: number) {
+    this.http.get<any[]>(`http://localhost:3000/api/terv/${foldID}`).subscribe(tervs => {
+        console.log('Tervek:', tervs);
+        this.tervSubject.next(tervs); 
+      });
+  }
+  getTerv() {
+    return this.tervSubject.value;
+  }
+
+  private loadFromStorage() {
+    const data = localStorage.getItem(terv);
+    return data ? JSON.parse(data) : null;
+  }
 }
