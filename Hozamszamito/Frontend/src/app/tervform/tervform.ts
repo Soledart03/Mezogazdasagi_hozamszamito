747c0adf2915d6689204e6ec4a9212333d9c87f7
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2026-01-25 15:29:10 +0100

    v_foldfel_nw_bugless

--- Hozamszamito/Frontend/src/app/tervform/tervform.ts
+++ Hozamszamito/Frontend/src/app/tervform/tervform.ts
@@ -12,18 +12,33 @@ import { Tervservice } from '../tervservice';
 export class Tervform implements OnInit {
 
 
-  constructor( private tervserv:Tervservice){}
+  constructor( private tervserv:Tervservice,private foldserv:Foldservice, private gazdaser:Gazdaservice){}
   foldId: number;
+  gazdaId: number;
+  selectedOptionId: number | null = null;
+  foldek: any = [];
   ngOnInit() {
-    
+      this.gazdaser.gazda$.subscribe(gazda => {
+    this.gazdaId = gazda?.id ?? 0;
+
+    if (this.gazdaId !== 0) {
+      this.foldserv.loadFoldsByGazdaId(this.gazdaId);
+    }
+
+    console.log('gazdaId:', this.gazdaId);
+  });
+
+    this.foldserv.fold$.subscribe(folds => {
+    this.foldId = folds?.[0]?.id ?? 0;
+  });
     
   }
   ujterv = {fold_id:0,noveny_id:0,kiv_vetoid:0,kiv_mutrid:0,vetes_idopont:'',tomeg:0,osszeg:0}
   tervHozzaad(){
     this.ujterv.fold_id = this.foldId;
     console.log('MENTÉS ELŐTT:', this.ujterv);
-    this.tervserv.addFold(this.ujfold);
-    this.ujfold = {terulet:'',muvelesi_ag:'',helyrajzi_szam:'',elozo_evi_hasznositas:'',gazda_id:0}
+    this.tervserv.addTerv(this.ujterv);
+    this.ujterv = {fold_id:0,noveny_id:0,kiv_vetoid:0,kiv_mutrid:0,vetes_idopont:'',tomeg:0,osszeg:0}
     this.closeMenu();
   }
   @Input()IsMenu: boolean = false;
