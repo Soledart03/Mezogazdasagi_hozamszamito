73b820db0977bdb9115c5493f4680545034cd690
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2026-01-26 12:47:35 +0100

    v_PDF_work

--- Hozamszamito/Frontend/src/app/terv/terv.ts
+++ Hozamszamito/Frontend/src/app/terv/terv.ts
@@ -4,14 +4,17 @@ import { Gazdaservice } from '../gazdaservice';
 import { Tervservice } from '../tervservice';
 import { Observable } from 'rxjs';
 import { Foldservice } from '../foldservice';
-
+import { DEJAVU_BASE64 } from '../../assets/fonts/dejavu';
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 @Component({
   selector: 'app-terv',
   standalone: false,
   templateUrl: './terv.html',
   styleUrl: './terv.css',
 })
-export class Terv {
+
+export class Terv implements OnInit {
 constructor(private app:App,private gazdaser:Gazdaservice,private tervser:Tervservice,private foldser:Foldservice){}
 terv$!: Observable<any[]>;
 folds$!: Observable<any[]>;
@@ -106,14 +109,112 @@ getNoveny(terv: any) {
 Vegosszeg(terv:any){
   
 }
-/*
-getFirstnovinp(terv:any){
-  return this.vetomagok.find(
-    (n: any) => n.id === terv.noveny_id
-  );
+
+addLabelValue(doc: jsPDF, label: string, value: string, x: number, y: number) {
+  doc.setFont('DejaVu');
+  doc.text(label, x, y);
+  doc.setFont('DejaVu', 'normal');
+  doc.text(value ?? '-', x + 55, y);
 }
-  */
- 
+PDFgen(terv: any) {
+   
+  //'p', 'mm', 'a4'
+  const doc = new jsPDF();
+(doc as any).addFileToVFS(
+  'DejaVuSans.ttf',
+  DEJAVU_BASE64
+);
+(doc as any).addFont(
+  'DejaVuSans.ttf',
+  'DejaVu',
+  'normal'
+);
+doc.setFont('DejaVu');
+  const kepUrl = this.getNoveny(terv)?.kep;
+  const fold = this.getFold(terv);
+  const noveny = this.getNoveny(terv);
+  const vetomag = this.getKivVetomag(terv);
+  const mutragya = this.getKivMutragya(terv);
+
+  
+  doc.setFontSize(20);
+  doc.text('Terv adatok', 105, 15, { align: 'center' });
+
+  doc.setDrawColor(0);
+  doc.line(10, 20, 200, 20);
+
+  doc.setFontSize(11);
+
+  let y = 30;
+
+  this.addLabelValue(doc, 'Helyrajzi szám:', fold?.helyrajzi_szam, 10, y); y += 8;
+  this.addLabelValue(doc, 'Vetés időpont:', terv.vetes_idopont.split('T')[0], 10, y); y += 8;
+  this.addLabelValue(doc, 'Növény:', noveny?.nnev, 10, y); y += 8;
+  this.addLabelValue(doc, 'Terület (ha):', fold?.terulet?.toString(), 10, y); y += 8;
+  this.addLabelValue(doc, 'Művelési ág:', fold?.muvelesi_ag, 10, y); y += 8;
+  this.addLabelValue(doc, 'Vetőmag:', vetomag?.fajta, 10, y); y += 8;
+  this.addLabelValue(doc, 'Vetőmag tömeg:', terv.tomeg + ' kg', 10, y); y += 8;
+
+  if (this.mutrvane) {
+    this.addLabelValue(doc, 'Műtrágya:', mutragya?.fajta.split(" ").slice(0,2).join(" "), 10, y); y += 8;
+  }
+
+  
+    
+  autoTable(doc, {
+    startY: y + 5,
+    head: [['Tétel', 'Mennyiség', 'Egységár (Ft)', 'Összeg (Ft)']],
+    body: [
+      [
+        'Vetőmag',
+        `${terv.tomeg} kg`,
+        vetomag?.ar ?? '-',
+        vetomag ? (terv.tomeg * vetomag.ar).toLocaleString() : '-'
+      ],
+      mutragya ? [
+        'Műtrágya',
+        mutragya?.fajta.split(" ").slice(-1),
+        mutragya.ar,
+        (mutragya.tomeg * mutragya.ar).toLocaleString()
+      ] : []
+    ].filter(r => r.length),
+    styles: {
+       font: 'DejaVu',
+      fontSize: 10,
+      cellPadding: 3
+    },
+    headStyles: {
+      font: 'DejaVu',
+      fillColor: [22, 160, 133]
+    }
+  });
+
+  const finalY = (doc as any).lastAutoTable.finalY + 10;
+  doc.setFontSize(12);
+  doc.setFont(undefined, 'bold');
+  doc.text(
+    `Összes költség: ${terv.osszeg?.toLocaleString() ?? '---'} Ft`,
+    10,
+    finalY
+  );
+
+ const fileName = this.getFold(terv)?.helyrajzi_szam;
+  
+
+fetch(kepUrl)
+    .then(res => res.blob())
+    .then(blob => new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    }))
+    .then(base64 => {
+      doc.addImage(base64, 'PNG', 110, 7, 100, 100);
+      doc.save(fileName); 
+    })
+}
+
 openEdit(terv: any) {
   
   this.selectedTerv = { ...terv };
