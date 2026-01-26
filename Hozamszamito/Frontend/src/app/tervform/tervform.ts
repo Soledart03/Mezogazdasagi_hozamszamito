9c4d7859b0fe09af3f0d9f164fe8d5fcbef3afa6
Raudin <107205571+Horrorbanana@users.noreply.github.com>
2026-01-26 17:23:05 +0100

    v_tervform_scroller

--- Hozamszamito/Frontend/src/app/tervform/tervform.ts
+++ Hozamszamito/Frontend/src/app/tervform/tervform.ts
@@ -17,6 +17,10 @@ export class Tervform implements OnInit {
   gazdaId: number;
   selectedOptionId: number | null = null;
   foldek: any = [];
+  novenyek: any = [];
+  szurtmagok:any = [];
+  vetomagok:any = [];
+  mutragyak:any = [];
   ngOnInit() {
       this.gazdaser.gazda$.subscribe(gazda => {
     this.gazdaId = gazda?.id ?? 0;
@@ -39,7 +43,32 @@ export class Tervform implements OnInit {
       this.tervserv.loadTervByFold(foldId);
     }
   });
+  this.tervserv.loadNoveny().subscribe(noveny => {
+    this.novenyek = noveny;
+  });
+  this.tervserv.loadConnNovinp().subscribe(vetomag => {
+    this.vetomagok = vetomag;
+    
+  });
+  this.tervserv.loadMutragya().subscribe(mutragya => {
+    this.mutragyak = mutragya;
+    console.log('mutragya:',this.mutragyak);
+  });
   }
+  novnev = "";
+  vetoszures(noveny:any){
+    if (!this.vetomagok || this.vetomagok.length === 0) {
+    console.warn('Vetőmagok még nincsenek betöltve');
+    return;
+  }
+  console.log('VETOSZURES MENTÉS ELŐTT:',noveny);
+  console.log('VETOSZURES MENTÉS ELŐTT:',this.vetomagok);
+    this.szurtmagok = this.vetomagok.filter(
+    (vm: any) => vm.nnev === noveny.nnev
+  );
+    console.log('MENTÉS ELŐTT:', this.szurtmagok);
+  }
+
   ujterv = {fold_id:0,noveny_id:0,kiv_vetoid:0,kiv_mutrid:0,vetes_idopont:'',tomeg:0,osszeg:0}
   tervHozzaad(){
     this.ujterv.fold_id = this.foldId;
